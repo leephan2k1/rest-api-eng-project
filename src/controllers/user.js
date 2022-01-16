4ededf1a991b05bbfbe1c75586b3a9177fd59555
lee phan <pttl2k1@gmail.com>
2022-01-16 17:58:45 +0700

    refactor callback to promise

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,24 +1,28 @@
 const User = require("../models/User");
 
 const index = (req, res, next) => {
-  User.find({}, (err, users) => {
-    if (err) next(err);
-    return res.status(200).json({
-      users,
-    });
-  });
+  User.find({})
+    .then((users) => {
+      return res.status(200).json({
+        users,
+      });
+    })
+    .catch((error) => next(error));
 };
 
 const newUser = (req, res, next) => {
   //create object user
   const newUsr = new User(req.body);
-  newUsr.save((err, usr) => {
-    if (err) console.err(err);
-    console.log("User saved!", usr);
-    return res.status(201).json({
-      usr,
+  newUsr
+    .save()
+    .then((usr) => {
+      return res.status(200).json({
+        usr,
+      });
+    })
+    .catch((err) => {
+      next(err);
     });
-  });
 };
 
 module.exports = {
